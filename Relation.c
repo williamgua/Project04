3fef47aba8cc7e55158c60ecbbbc122e2e7fda84
williamgua <williamgkh98@gmail.com>
2018-04-07 17:45:10 -0400

    Relation with equal func

--- Relation.c
+++ Relation.c
@@ -12,32 +12,70 @@ CSG CSG_new(char course[5], int studentId, char grade[2]) {
 }
 
 SNAP SNAP_new(int studentId, char name[15], char address[15], char phone[9]) {
-  SNAP snap = (SNAP)malloc(sizeof(SNAP));
-  snap->studentId = studentId;
-  strcpy(snap->name, name);
-  strcpy(snap->address, address);
-  strcpy(snap->phone, phone);
-  return snap;
+	SNAP snap = (SNAP)malloc(sizeof(SNAP));
+	snap->studentId = studentId;
+	strcpy(snap->name, name);
+	strcpy(snap->address, address);
+	strcpy(snap->phone, phone);
+	return snap;
 }
 
 CP CP_new(char course[5], char prereq[5]) {
-  CP cp = (CP)malloc(sizeof(CP));
-  strcpy(cp->course, course);
-  strcpy(cp->prereq, prereq);
-  return cp;
+	CP cp = (CP)malloc(sizeof(CP));
+	strcpy(cp->course, course);
+	strcpy(cp->prereq, prereq);
+	return cp;
 }
 
 CR CR_new(char course[5], char room[15]) {
-  CR cr = (CR)malloc(sizeof(CR));
-  strcpy(cr->course, course);
-  strcpy(cr->room, room);
-  return cr;
+	CR cr = (CR)malloc(sizeof(CR));
+ 	strcpy(cr->course, course);
+	strcpy(cr->room, room);
+	return cr;
 }
 
 CDH CDH_new(char course[5], char day[2], char hour[4]) {
-  CDH cdh = (CDH)malloc(sizeof(CDH));
-  strcpy(cdh->course,course);
-  strcpy(cdh->day,day);
-  strcpy(cdh->hour,hour);
-  return cdh;
+	CDH cdh = (CDH)malloc(sizeof(CDH));
+  	strcpy(cdh->course,course);
+	strcpy(cdh->day,day);
+	strcpy(cdh->hour,hour);
+  	return cdh;
+}
+
+int CSG_equal(CSG r1, CSG r2) {
+	if(r1 == NULL || r2 == NULL) return 0; 
+	if(r1->studentId != r2->studentId) return 0; //need to be string and deal with * ??
+	if(!(strcmp(r1->course, r2->course) == 0) && !((strcmp(r1->course, "*") == 0) || (strcmp(r2->course, "*") == 0))) return 0;
+	if(!(strcmp(r1->grade, r2->grade) == 0) && !((strcmp(r1->grade, "*") == 0) || (strcmp(r2->grade, "*") == 0))) return 0;
+	return 1;
+}
+
+int SNAP_equal(SNAP r1, SNAP r2) {
+	if(r1 == NULL || r2 == NULL) return 0; 
+	if(r1->studentId != r2->studentId) return 0; //need to be string and deal with * ??
+	if(!(strcmp(r1->name, r2->name) == 0) && !((strcmp(r1->name, "*") == 0) || (strcmp(r2->name, "*") == 0))) return 0;
+	if(!(strcmp(r1->address, r2->address) == 0) && !((strcmp(r1->address, "*") == 0) || (strcmp(r2->address, "*") == 0))) return 0;
+	if(!(strcmp(r1->phone, r2->phone) == 0) && !((strcmp(r1->phone, "*") == 0) || (strcmp(r2->phone, "*") == 0))) return 0;
+	return 1;
+}
+
+int CP_equal(CP r1, CP r2) {
+	if(r1 == NULL || r2 == NULL) return 0; 
+	if(!(strcmp(r1->prereq, r2->prereq) == 0) && !((strcmp(r1->prereq, "*") == 0) || (strcmp(r2->prereq, "*") == 0))) return 0;
+	if(!(strcmp(r1->course, r2->course) == 0) && !((strcmp(r1->course, "*") == 0) || (strcmp(r2->course, "*") == 0))) return 0;
+	return 1;
+}
+
+int CR_equal(CR r1, CR r2) {
+	if(r1 == NULL || r2 == NULL) return 0; 
+	if(!(strcmp(r1->room, r2->room) == 0) && !((strcmp(r1->room, "*") == 0) || (strcmp(r2->room, "*") == 0))) return 0;
+	if(!(strcmp(r1->course, r2->course) == 0) && !((strcmp(r1->course, "*") == 0) || (strcmp(r2->course, "*") == 0))) return 0;
+	return 1;
+}
+
+int CDH_equal(CDH r1, CDH r2) {
+	if(r1 == NULL || r2 == NULL) return 0;
+	if(!(strcmp(r1->day, r2->day) == 0) && !((strcmp(r1->day, "*") == 0) || (strcmp(r2->day, "*") == 0))) return 0;
+	if(!(strcmp(r1->hour, r2->hour) == 0) && !((strcmp(r1->hour, "*") == 0) || (strcmp(r2->hour, "*") == 0))) return 0;
+	return 1;
 }
--- Relation.h
+++ Relation.h
@@ -38,5 +38,10 @@ extern SNAP SNAP_new(int studentId, char* name, char* address, char* phone);
 extern CP CP_new(char* course, char* prereq);
 extern CR CR_new(char* course, char* room);
 extern CDH CDH_new(char* course, char* day, char* hour);
+extern int CSG_equal(CSG r1, CSG r2);
+extern int SNAP_equal(SNAP r1, SNAP r2);
+extern int CP_equal(CP r1, CP r2);
+extern int CR_equal(CR r1, CR r2);
+extern int CDH_equal(CDH r1, CDH r2);
 
 #endif
