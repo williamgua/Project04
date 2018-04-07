00429ad9a24619c0456a596f1b563c182cd46e07
wendybalaja <mzhou26@u.rochester.edu>
2018-04-07 19:21:32 -0400

    fked up updates

--- Database.c
+++ Database.c
@@ -20,12 +20,52 @@ void insertCSG(CSG *csg, Database *d){
 	int index = hash(charToInt(csg->course));
 
 	if (d->CSGtb[index] == NULL){
-		CSGtb[index] = csg;
+		d->CSGtb[index] = csg;
 	}else{
-		csg->next = 
+		csg->next = d->CSGtb[index];
+        d->CSGtb[index] = csg;
 	}
 }
 void insertSNAP(SNAP *snap, Database *d);
 void insertCP(CP *cp, Database *d );
 void insertCR (CR *cr, Database *d);
 void insertCDH (CDH *cdh, Database *d); 
+
+void deleteCSG (CSG csg, Database d){
+    int index = hash(charToInt(csg->course));
+    if (d->CSGtb[index] == NULL){
+        return;
+    }else{
+
+        if(CDH_equal(d->CSGtb[index],csg) == 1){
+            d->CSGtb[index] = d->CSGtb[index]->next;
+        }
+        else{
+            CSG csgPtr = d->CSGtb[index]->next;
+            while (CDH_equal(csgPtr, csg) == 0 && csgPtr->next != NULL){
+                csgPtr = csgPtr->next;
+            }
+        }
+        }
+
+
+    }
+
+CSG* lookupCSG (CSG csg, Database d) {
+    int index = hash(charToInt(csg->course));
+    CSG* resultList = (CSG*)malloc(sizeof(CSG*));
+    int i = 0;
+
+    if(d->CSGtb[index] == NULL){
+        return NULL;
+    }
+    else {
+        for(CSG iter = d->CSGtb[index]; iter !=NULL; iter=iter->next){
+            if (CSG_equal(csg,iter) ==1){
+                resultList[i] = iter;
+                i++;
+            }
+        }
+    }
+    return resultList;
+}
