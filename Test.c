6a6bab98afaae493c8a3ea8bcc09b056d911656b
Valerie <xwang145@u.rochester.edu>
2018-04-07 21:53:00 -0400

    fixed id

--- Test.c
+++ Test.c
@@ -4,10 +4,10 @@
 int main() {
     Database test = Database_new();
 
-    insertCSG(test, CSG_new("CSC101", "12345", "B+"));
-    insertCSG(test, CSG_new("CSC101", "12345", "F"));
-    insertCSG(test, CSG_new("CSC101", "12346", "B-"));
-    insertCSG(test, CSG_new("CSC103", "12347", "B"));
+    insertCSG(test, CSG_new("CSC101", 12345, "B+"));
+    insertCSG(test, CSG_new("CSC101", 12345, "F"));
+    insertCSG(test, CSG_new("CSC101", 12346, "B-"));
+    insertCSG(test, CSG_new("CSC103", 12347, "B"));
 
     print_CSG_DB(test);
 }
