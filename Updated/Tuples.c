e0e238b2c707b8ca65f176af86754fb9a82ff060
williamgua <williamgkh98@gmail.com>
2018-04-09 22:22:12 -0400

    Init revised proj

---
+++ Updated/Tuples.c
@@ -0,0 +1,82 @@
+#include "Tuples.h"
+
+
+int equal_CSG(CSG* c1, CSG* c2){
+    if(c1 == NULL || c2 == NULL) return 0; 
+    if(!(strcmp(c1->StudentId, c2->StudentId) == 0) && !((strcmp(c1->StudentId, "*") == 0) || (strcmp(c2->StudentId, "*") == 0))) return 0;
+    if(!(strcmp(c1->course, c2->course) == 0) && !((strcmp(c1->course, "*") == 0) || (strcmp(c2->course, "*") == 0))) return 0;
+    if(!(strcmp(c1->grade, c2->grade) == 0) && !((strcmp(c1->grade, "*") == 0) || (strcmp(c2->grade, "*") == 0))) return 0;
+    return 1;
+}
+
+int equal_SNAP(SNAP* s1, SNAP* s2){
+    if (s1 == NULL || s2 == NULL) return 0;
+    if(!(strcmp(s1->StudentId, s2->StudentId) == 0) && !((strcmp(s1->StudentId, "*") == 0) || (strcmp(s2->StudentId, "*") == 0))) return 0;
+    if (!(strcmp(s1->Name,s2->Name) == 0) && !((strcmp(s1->Name,"*") == 0) || (strcmp(s2->Name,"*") == 0))) return 0;
+    if (!(strcmp(s1->Address,s2->Address) == 0) && !((strcmp(s1->Address,"*") == 0) || (strcmp(s2->Address,"*") == 0))) return 0;
+    if (!(strcmp(s1->Phone, s2->Phone) == 0) && !((strcmp(s1->Phone,"*") == 0) || (strcmp(s2->Phone,"*") == 0))) return 0;
+    return 1;
+}
+
+int equal_CP(CP* c1, CP* c2){
+    if(c1 == NULL || c2 == NULL) return 0; 
+    if(!(strcmp(c1->Prerequisite, c2->Prerequisite) == 0) && !((strcmp(c1->Prerequisite, "*") == 0) || (strcmp(c2->Prerequisite, "*") == 0))) return 0;
+    if(!(strcmp(c1->Course, c2->Course) == 0) && !((strcmp(c1->Course, "*") == 0) || (strcmp(c2->Course, "*") == 0))) return 0;
+    return 1;
+}
+
+int equal_CDH(CDH* r1, CDH* r2){
+    if(r1 == NULL || r2 == NULL) return 0;
+    if(!(strcmp(r1->Day, r2->Day) == 0) && !((strcmp(r1->Day, "*") == 0) || (strcmp(r2->Day, "*") == 0))) return 0;
+    if(!(strcmp(r1->Hour, r2->Hour) == 0) && !((strcmp(r1->Hour, "*") == 0) || (strcmp(r2->Hour, "*") == 0))) return 0;
+    if(!(strcmp(r1->Course, r2->Course) == 0) && !((strcmp(r1->Course, "*") == 0) || (strcmp(r2->Course, "*") == 0))) return 0;
+    return 1;
+}
+
+int equal_CR(CR* c1, CR* c2){
+    if(c1 == NULL || c2 == NULL) return 0; 
+    if(!(strcmp(c1->Room, c2->Room) == 0) && !((strcmp(c1->Room, "*") == 0) || (strcmp(c2->Room, "*") == 0))) return 0;
+    if(!(strcmp(c1->Course, c2->Course) == 0) && !((strcmp(c1->Course, "*") == 0) || (strcmp(c2->Course, "*") == 0))) return 0;
+    return 1;
+}
+
+CSG* CSG_new(char* Course, char* StudentId, char* Grade){
+    CSG* new = (CSG*)malloc(sizeof(CSG));
+    strcpy(new->StudentId, StudentId);
+    strcpy(new->Course, Course);
+    strcpy(new->Grade, Grade);
+    return new;
+}
+
+SNAP* SNAP_new(char* StudentId, char* Name, char* Address, char* Phone){
+    SNAP* new = (SNAP*)malloc(sizeof(SNAP));
+    strcpy(new->StudentId, StudentId);
+    strcpy(new->Name, Name);
+    strcpy(new->Address, Address);
+    strcpy(new->Phone, Phone);
+    return new;
+}
+
+CP* CP_new(char* Course, char* Prerequisite){
+    CP* new = (CP*)malloc(sizeof(CP));
+    strcpy(new->Course, Course);
+    strcpy(new->Prerequisite, Prerequisite);
+    return new;
+}
+
+CDH* CDH_new(char* Course, char* Day, char* Hour){
+    CDH* new = (CDH*)malloc(sizeof(CDH));
+    strcpy(new->Course, Course);
+    strcpy(new->Day, Day);
+    strcpy(new->Hour, Hour);
+    return new;
+}
+
+CR* CR_new(char* Course, char* Room){
+    CR* new = (CR*)malloc(sizeof(CR));
+    strcpy(new->Course, Course);
+    strcpy(new->Room, Room);
+    return new;
+}
+
+
---
+++ Updated/Tuples.h
@@ -0,0 +1,70 @@
+#ifndef _tuples_h
+#define _tuples_h
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct CSG *CSGLIST;
+
+typedef struct CSG{
+    char Course[5];
+    char StudentId[10];
+    char Grade[2];
+    CSGLIST next;
+} CSG;
+
+
+//studentid-name-address-phone
+typedef struct SNAP *SNAPLIST;
+typedef struct SNAP {
+    char StudentId[10];
+    char Name[15];
+    char Address[15];
+    char Phone[10];
+    SNAPLIST next;
+} SNAP;
+
+
+//course-prerequisite
+typedef struct CP *CPLIST;
+typedef struct CP {
+    char Course[5];
+    char Prerequisite[5];
+    CPLIST next;
+} CP;
+
+
+//course-day-hour
+typedef struct CDH *CDHLIST;
+typedef struct CDH {
+    char Course[5];
+    char Day[2];
+    char Hour[4];
+    CDHLIST next;
+} CDH;
+
+//course-room
+typedef struct CR *CRLIST;
+typedef struct CR {
+    char Course[5];
+    char Room[15];
+    CRLIST next;
+} CR;
+
+
+/* Equal functions */
+extern int equal_SNAP(SNAP* s1, SNAP* s2);
+extern int equal_CSG(CSG* c1, CSG* c2);
+extern int equal_CP(CP* c1, CP* c2);
+extern int equal_CDH(CDH* c1, CDH* c2);
+extern int equal_CR(CR* c1,CR* c2);
+
+/* New functions */
+extern SNAP* SNAP_new(char* StudentId, char* Name, char* Address, char* Phone);
+extern CSG* CSG_new(char* Course, char* StudentId, char* Grade);
+extern CP* CP_new(char* Course, char* Prerequisite);
+extern CDH* CDH_new(char* Course, char* Day, char* Hour);
+extern CR* CR_new(char* Course, char* Room);
+
+#endif
