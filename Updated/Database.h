d78c421efe5177676bd2c9843ba922a9af2e90cd
Valerie <xwang145@u.rochester.edu>
2018-04-11 16:30:59 -0400

    no

--- Updated/Database.h
+++ Updated/Database.h
@@ -9,35 +9,35 @@ typedef struct Database{
     CR* CRtb[1009];
 } Database;
 
-/* New database function */
+// Create a new database
 Database* Database_new();
 
-/* Hashing functions */
+// Hashing functions
 int hash(int input);
 int strToInt (char* input);
 
-/* Insert functions */
+// Insert
 void insert_SNAP(Database* D, SNAP* toInsert);
 void insert_CSG(Database* D, CSG* toInsert);
 void insert_CP(Database* D, CP* toInsert);
 void insert_CDH(Database* D, CDH* toInsert);
 void insert_CR(Database* D, CR* toInsert);
 
-/* Delete functions */
+// Delete
 void delete_SNAP(Database* D, SNAP* toDelete);
 void delete_CSG(Database* D, CSG* toDelete);
 void delete_CP(Database* D, CP* toDelete);
 void delete_CDH(Database* D, CDH* toDelete);
 void delete_CR(Database* D, CR* toDelete);
 
-/* Lookup functions */
+// Lookup
 SNAP** lookup_SNAP(Database* D, SNAP* toLookup);
 CSG** lookup_CSG(Database* D, CSG* toLookup);
 CP** lookup_CP(Database* D, CP* toLookup);
 CDH** lookup_CDH(Database* D, CDH* toLookup);
 CR** lookup_CR(Database* D, CR* toLookup);
 
-/* Print functions */
+// Print functions
 void print_CSG(CSGLIST* csglist);
 void print_SNAP(SNAPLIST* snaplist);
 void print_CP(CPLIST* cplist);
