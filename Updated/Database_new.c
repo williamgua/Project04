8f9be9baebfe5dc280e76a08f2b4a796ea894251
Valerie <xwang145@u.rochester.edu>
2018-04-22 16:18:01 -0400

    g

--- Updated/Database_new.c
+++ Updated/Database_new.c
@@ -544,6 +544,236 @@ void projection_CSG(CSG** csg) {
     }
 }
 
+//void importDB(char* fileName) {
+//    FILE *fp;
+//    char buf[100];
+//    fp = fopen(filename, "r");
+//    if (fp == NULL) {
+//        perror("Error: Cannot Open File");
+//        return;
+//    }
+//    while(fgets(buf, 100, fp) != NULL) {
+//        int length = strlen(buf);
+//        char input[length];
+//        strcpy(input, buf);
+//        char* token = strtok(input, " ");
+//        if (strcmp(token, "CSG") == 0) {
+//            for(int i=5; i<length; i++) {
+//                input[i-5] = input[i];
+//            }
+//            for(int i=length-1; i>length-5; i--) {
+//                input[i] = " ";
+//            }
+//            length -= 5;
+//            char *course = strtok(input, " ");
+//            int len1 = strlen(course)+1;
+//            for(int i=len1; i<length; i++) {
+//                input[i-len1] = input[i];
+//            }
+//            for(int i=length-1; i>length-len1; i--) {
+//                input[i] = " ";
+//            }
+//            length -= len1;
+//            char *StudentId = strtok(input, " ");
+//            int len2 = strlen(StudentId)+1;
+//            for(int i=len2; i<length; i++) {
+//                input[i-len2] = input[i];
+//            }
+//            for(int i=length-1; i>length-len2; i--) {
+//                input[i] = " ";
+//            }
+//            length -= len2;
+//            char *Grade = strtok(input, " ");
+//            int len3 = strlen(Grade)+1;
+//            for(int i=len3; i<length; i++) {
+//                input[i-len3] = input[i];
+//            }
+//            for(int i=length-1; i>length-len3; i--) {
+//                input[i] = " ";
+//            }
+//            length -= len3;
+//            insert_CSG(CSG_new(Course, StudentId, Grade), csgtable);
+//        } else if (strcmp(token, "SNAP") == 0) {
+//            for(int i=6; i<length; i++) {
+//                input[i-6] = input[i];
+//            }
+//            for(int i=length-1; i>length-6; i--) {
+//                input[i] = " ";
+//            }
+//            length -= 6;
+//            char* StudentId = strtok(input, " ");
+//            int len2 = strlen(StudentId)+1;
+//            for(int i=len2; i<length; i++) {
+//                input[i-len2] = input[i];
+//            }
+//            for(int i=length-1; i>length-len2; i--) {
+//                input[i] = " ";
+//            }
+//            length -= len2;
+//            char* Name = strtok(input, " ");
+//            int len1 = strlen(Name)+1;
+//            for(int i=len1; i<length; i++) {
+//                input[i-len1] = input[i];
+//            }
+//            for(int i=length-1; i>length-len1; i--) {
+//                input[i] = " ";
+//            }
+//            length -= len1;
+//            char* Address = strtok(input, " ");
+//            int len3 = strlen(Address)+1;
+//            for(int i=len3; i<length; i++) {
+//                input[i-len3] = input[i];
+//            }
+//            for(int i=length-1; i>length-len3; i--) {
+//                input[i] = " ";
+//            }
+//            length -= len3;
+//            char* Phone = strtok(input, " ");
+//            int len4 = strlen(Phone)+1;
+//            for(int i=len4; i<length; i++) {
+//                input[i-len4] = input[i];
+//            }
+//            for(int i=length-1; i>length-len4; i--) {
+//                input[i] = " ";
+//            }
+//            length -= len4;
+//            insert_SNAP(SNAP_new(StudentId, Name, Address, Phone), snaptable);
+//        } else if (strcmp(token, "CP") == 0) {
+//            for(int i=4; i<length; i++) {
+//                input[i-4] = input[i];
+//            }
+//            for(int i=length-1; i>length-4; i--) {
+//                input[i] = " ";
+//            }
+//            length -= 4;
+//            char* Course = strtok(input, " ");
+//            int len1 = strlen(Course)+1;
+//            for(int i=len1; i<length; i++) {
+//                input[i-len1] = input[i];
+//            }
+//            for(int i=length-1; i>length-len1; i--) {
+//                input[i] = " ";
+//            }
+//            length -= len1;
+//            char* Prerequisite = strtok(input, " ");
+//            int len2 = strlen(Prerequisite)+1;
+//            for(int i=len2; i<length; i++) {
+//                input[i-len2] = input[i];
+//            }
+//            for(int i=length-1; i>length-len2; i--) {
+//                input[i] = " ";
+//            }
+//            length -= len2;
+//            insert_CP(CP_new(Course, Prerequisite), cptable);
+//        } else if (strcmp(token, "CDH") == 0) {
+//            for(int i=5; i<length; i++) {
+//                input[i-5] = input[i];
+//            }
+//            for(int i=length-1; i>length-5; i--) {
+//                input[i] = " ";
+//            }
+//            length -= 5;
+//            char* Course = strtok(input, " ");
+//            int len1 = strlen(Course)+1;
+//            for(int i=len1; i<length; i++) {
+//                input[i-len1] = input[i];
+//            }
+//            for(int i=length-1; i>length-len1; i--) {
+//                input[i] = " ";
+//            }
+//            length -= len1;
+//            char* Day = strtok(input, " ");
+//            char* Hour = strtok(input, " ");
+//            insert_CDH(CDH_new(Course, Day, Hour), cdhtable);
+//        } else if (strcmp(token, "CR") == 0) {
+//            for(int i=4; i<length; i++) {
+//                input[i-4] = input[i];
+//            }
+//            for(int i=length-1; i>length-4; i--) {
+//                input[i] = " ";
+//            }
+//            length -= 4;
+//            char* Course = strtok(input, " ");
+//            int len1 = strlen(Course)+1;
+//            for(int i=len1; i<length; i++) {
+//                input[i-len1] = input[i];
+//            }
+//            for(int i=length-1; i>length-len1; i--) {
+//                input[i] = " ";
+//            }
+//            length -= len1;
+//            char* Room = strtok(input, " ");
+//            int len2 = strlen(Room)+1;
+//            for(int i=len2; i<length; i++) {
+//                input[i-len2] = input[i];
+//            }
+//            for(int i=length-1; i>length-len2; i--) {
+//                input[i] = " ";
+//            }
+//            length -= len2;
+//            insert_CR(CR_new(Course, Room), crtable);
+//        } else {
+//            perror("Error");
+//        }
+//    }
+//    if(fclose(fp) != 0) {
+//        perror("Error: Cannot Close File");
+//    }
+//    
+//}
+
+//void exportDB(char *fileName) {
+//    FILE *fp;
+//    fp = fopen(filename, "w");
+//    if (fp == NULL) {
+//        perror("Error: Cannot Open File");
+//    } else {
+//        int i;
+//        //CSG
+//        for (i=0; i<1009; i++) {
+//            CSG *current = CSGtb[i];
+//            while (current != NULL) {
+//                fprintf(fp, "CSG \n%s %d %s\n", current->Course, current->StudentId, current->Grade);
+//                current = current->next;
+//            }
+//        }
+//        //SNAP
+//        for (i=0; i<1009; i++) {
+//            SNAP *current = SNAPtb[i];
+//            while(current != NULL) {
+//                fprintf(fp, "SNAP \n%d %s %s %d\n", current->StudentId, current->Name, current->Address, current->Phone);
+//                current = current->next;
+//            }
+//        }
+//        //CP
+//        for (i=0; i<1009; i++) {
+//            CP *current = CPtb[i];
+//            while(current != NULL) {
+//                fprintf(fp, "CP \n%s %s\n", current->Course, current->Prerequisite);
+//                current = current->next;
+//            }
+//        }
+//        //CDH
+//        for (i=0; i<1009; i++) {
+//            CDH *current = CDHtb[i];
+//            while(current != NULL) {
+//                fprintf(fp, "CDH \n%s %s %s\n", current->Course, current->Day, current->Hour);
+//                current = current->next;
+//            }
+//        }
+//        //CR
+//        for (i=0; i<1009; i++) {
+//            CR *current = CRtb[i];
+//            while(current != NULL) {
+//                fprintf(fp, "CR \n%s %s\n", current->Course, current->Room);
+//                current = current->next;
+//            }
+//        }
+//        if(fclose(fp) != 0) {
+//            perror("Error: Cannot Close File");
+//        }
+//    }
+//}
 
 int main() {
     CSGtb csgtable ;
@@ -602,6 +832,10 @@ int main() {
     insert_CR( CR_new("EE200", "25 Ohm Hall"), crtable);
     insert_CR( CR_new("PH100", "Newton Lab."), crtable);
 
+    //Import and Export
+//    exportDB("database.txt");
+//    importDB("database.txt");
+    
     //eg 8.2
     CSGptr* csglist = lookup_CSG(CSG_new("CS101", "12345", "*"), csgtable);
     CPptr* cplist = lookup_CP(CP_new("CSC205", "CS120"), cptable);
--- Updated/Database_new.h
+++ Updated/Database_new.h
@@ -3,37 +3,35 @@
 
 #include "Tuple.h"
 
-/* Hashing functions */
 int hash(int input);
 int strToInt(char* input);
 
-/* Insert functions */
 void insert_SNAP(SNAP* tuple, SNAPtb table);
 void insert_CSG(CSG* tuple, CSGtb table);
 void insert_CP(CP* tuple, CPtb table);
 void insert_CDH(CDH* tuple, CDHtb table);
 void insert_CR(CR* tuple, CRtb table);
 
-/* Delete functions */
 void delete_SNAP(SNAP* tuple, SNAPtb table);
 void delete_CSG(CSG* tuple, CSGtb table);
 void delete_CP(CP* tuple, CPtb table);
 void delete_CDH(CDH* tuple, CDHtb table);
 void delete_CR(CR* tuple, CRtb table);
 
-/* Lookup functions */
 SNAP** lookup_SNAP(SNAP* tuple, SNAPtb table);
 CSG** lookup_CSG(CSG* tuple, CSGtb table);
 CP** lookup_CP(CP* tuple, CPtb table);
 CDH** lookup_CDH(CDH* tuple, CDHtb table);
 CR** lookup_CR(CR* tuple, CRtb table);
 
-/* Print functions */
 void print_CSG(CSGptr* list);
 void print_SNAP(SNAPptr* list);
 void print_CP(CPptr* list);
 void print_CDH(CDHptr* list);
 void print_CR(CRptr* list);
 void print_CRDH(CRDHptr* list);
+
+extern void importDB(char* fileName);
+extern void exportDB(char* fileName);
 
 #endif
